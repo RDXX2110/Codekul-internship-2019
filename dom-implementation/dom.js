9359c4bca04d2abf0088a1e64a9519124974b11d
RAJ <yorajpatil@github.com>
2019-06-17 10:40:17 +0530

    done!

---
+++ dom-implementation/dom.js
@@ -0,0 +1,22 @@
+// // console.dir(document);
+// // console.log(document.domain);
+// // console.log(document.URL);
+// // console.log(document.head);
+// // console.log(document.title);
+// // console.log(document.h1);
+
+// // var data=document.getElementById="headr-title";
+// // console.log(data);
+//  var items =document.getElementsByClassName="list-group";
+//  console.log(items[1]);
+//  items[1].style.backgraundColor ='yellow' ;
+// var data=document.getElementsByTagName="li";
+// console.log(data);
+// var header=document.querySelector("input");
+// input.value="hello"
+
+// var submit =document.querySelector('input[type="submit"]');
+// submit.value="send"
+var itemlist =document.querySelector('#items');
+//parentnode
+console.log(itemlist.parentNode);
